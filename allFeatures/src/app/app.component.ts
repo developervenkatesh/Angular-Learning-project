92563543b3900c058528c33fc33d510c29682a68
Doddi Venkateswarlu <Doddivenkateswarlu55@gmail.com>
2025-11-11 23:16:00 +0530

    added tailwind in our project

--- allFeatures/src/app/app.component.ts
+++ allFeatures/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginPageComponent } from '../loginPage/login-page/login-page.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [LoginPageComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'allFeatures';
